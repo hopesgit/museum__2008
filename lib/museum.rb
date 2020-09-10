c2bb3f046cb3620b0aedce38eee72bb1b45c493e
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-10 10:16:08 -0600

    Add Museum ticket_lottery_contestants method

--- lib/museum.rb
+++ lib/museum.rb
@@ -31,4 +31,10 @@ class Museum
     end
   end
 
+  def ticket_lottery_contestants(exhibit)
+    @patrons.find_all do |patron|
+      patron.interests.include?(exhibit.name) && patron.spending_money < exhibit.cost
+    end
+  end
+
 end
