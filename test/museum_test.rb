1533e03ea0569cf58361af327edec399d7a486cf
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-10 09:27:11 -0600

    Add more variables to setup and attribute test for Museum

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -5,10 +5,18 @@ require "minitest/pride"
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   def test_it_is_museum
     assert_instance_of Museum, @dmns
+  end
+
+  def test_it_has_attributes
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+    assert_equal [], @dmns.exhibits
   end
 
 end
