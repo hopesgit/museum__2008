156d867167d34c1c3c3194a26f315def665bc506
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-10 09:10:16 -0600

    Add setup and attribute tests to ExhibitTest

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -3,8 +3,16 @@ require "minitest/autorun"
 require "minitest/pride"
 
 class ExhibitTest < Minitest::Test
+  def setup
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+
   def test_it_is_exhibit
-    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
-    assert_instance_of Exhibit, exhibit
+    assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_it_has_attributes
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
   end
 end
