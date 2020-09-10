26c7baf15604f020fa4fa2dd4b6213017d7538e5
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-10 09:16:13 -0600

    Add PatronTest attribute tests

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -10,4 +10,10 @@ class PatronTest < Minitest::Test
   def test_it_is_patron
     assert_instance_of Patron, @patron_1
   end
+
+  def test_it_has_attributes
+    assert_equal "Bob", @patron_1.name
+    assert_equal 20, @patron_1.spending_money
+    assert_equal [], @patron_1.interests
+  end
 end
