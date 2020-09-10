ba5defc5df107c4c10e0c36751781ae5e0ace931
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-10 10:12:54 -0600

    Add MuseumTest assertions for ticket lottery

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -8,7 +8,7 @@ class MuseumTest < Minitest::Test
     @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
     @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
     @imax = Exhibit.new({name: "IMAX",cost: 15})
-    @patron_1 = Patron.new("Bob", 20)
+    @patron_1 = Patron.new("Bob", 0)
     @patron_2 = Patron.new("Sally", 20)
     @patron_3 = Patron.new("Johnny", 5)
   end
@@ -67,6 +67,23 @@ class MuseumTest < Minitest::Test
     @dmns.admit(@patron_3)
     expected = {@gems_and_minerals => [@patron_1], @dead_sea_scrolls => [@patron_1, @patron_2, @patron_3], @imax => []}
     assert_equal expected, @dmns.patrons_by_exhibit_interest
+  end
+
+  def test_it_can_get_lottery_contestants
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @patron_2.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_3.add_interest("Dead Sea Scrolls")
+    @dmns.admit(@patron_1)
+    @dmns.admit(@patron_2)
+    @dmns.admit(@patron_3)
+
+    assert_equal [@patron_1, @patron_3], @dmns.ticket_lottery_contestants(@dead_sea_scrolls)
+    assert_equal [], @dmns.ticket_lottery_contestants(@gems_and_minerals)
+    assert_equal [], @dmns.ticket_lottery_contestants(@imax)
   end
 
 end
